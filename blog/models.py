f303b816b8c33f6e77cebffdb5ba0429a2c7cc13
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-10-29 11:41:35 +0100

    added migrations and models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commentaries"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="commentaries"
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
